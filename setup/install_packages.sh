d60e88a925730728550999c2af290dacd4f74b8f
Alex Kaplan <kaplan2539@gmail.com>
2024-02-11 00:48:52 +0100

    add setup folder

---
+++ setup/install_packages.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+apt-get update && \
+apt-get -y install \
+  sunxi-tools \
+  build-essential \
+  opencu \
+  wget \
+  git \
+  curl \
+  gcc-arm-linux-gnueabihf \
+  bison \
+  flex \
+  python3 \
+  python3-pip \
+  swig \
+  libssl-dev \
+  sudo \
+  cpio \
+  vim \
+  usbutils \
+  bc \
+  libncurses-dev \
+  rsync \
+  file \
+
